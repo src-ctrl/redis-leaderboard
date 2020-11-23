0ef2d41412e4acba2d4df3e42dad248f28df21c9
Tom Hughes <33721645+src-ctrl@users.noreply.github.com>
2020-11-23 09:35:13 +0000

    added redis container

--- public/index.php
+++ public/index.php
@@ -5,6 +5,11 @@ use Symfony\Component\Dotenv\Dotenv;
 $dotenv = new Dotenv();
 $dotenv->load('../.env');
 
+$client = new Predis\Client('tcp://redis:6379');
+$client->set('foo', 'bar');
+$value = $client->get('foo');
+echo $value;
+
 try{
     $dbh = new pdo('mysql:host=' . $_ENV['DB_HOST']
                  . ':' . $_ENV['DB_PORT'] . ';'
