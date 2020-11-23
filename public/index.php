b610ee3f043b265c54f3f56310e5c3fea4409685
Tom Hughes <33721645+src-ctrl@users.noreply.github.com>
2020-11-23 00:09:49 +0000

    added dotenv and corrected mysql conn

--- public/index.php
+++ public/index.php
@@ -1,12 +1,23 @@
 <?php
-echo 'redis-leaderboard';
+require_once '../vendor/autoload.php';
+use Symfony\Component\Dotenv\Dotenv;
 
-$dsn = "mysql:host=db;dbname=redis-leaderboard";
-$user = "redis-leaderboard-user";
-$passwd = "password";
+$dotenv = new Dotenv();
+$dotenv->load('../.env');
 
-$pdo = new PDO($dsn, $user, $passwd);
-$stm = $pdo->query("SELECT * FROM places");
+try{
+    $dbh = new pdo('mysql:host=' . $_ENV['DB_HOST']
+                 . ':' . $_ENV['DB_PORT'] . ';'
+                 . 'dbname=' . $_ENV['DB_DATABASE'],
+                    $_ENV['DB_USERNAME'],
+                    $_ENV['DB_PASSWORD'],
+                    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}
+catch(PDOException $ex){
+    die(json_encode(array('outcome' => 'false', 'message' => 'Unable to connect to DB.')));
+}
+
+$stm = $dbh->query("SELECT * FROM places");
 $rows = $stm->fetchAll(PDO::FETCH_NUM);
 
-print_r($rows);
+echo json_encode($rows);
